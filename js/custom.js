29d926c6f71c55e9c725d5d7c4a2fde2bdf0dc4e
unknown <ahmedrony2101@gmail.com>
2021-10-12 16:10:32 +0600

    js hw_1

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+function addition(x,y){
+    var sum = x + y;
+    document.getElementsByClassName('result')[0].innerHTML = sum;
+}
+
+function subtraction(x,y){
+    var sub = x - y;
+    document.getElementsByClassName('result')[1].innerHTML = sub;
+
+}
+function multiplication(x,y){
+    var mlt = x * y;
+    document.getElementsByClassName('result')[2].innerHTML = mlt;
+
+}
